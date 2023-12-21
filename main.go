2e9abff164119ae7be30290a5c98065646a053a9
Brennon Gabriel de Oliveira <55529693+Brennon-Oliveira@users.noreply.github.com>
2023-12-21 20:39:47 +0000

    Adicionando coneccao com o banco de dados

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
-	println("Hello, World!")
+
+	log.Println("Starting academiQ-api")
+	connStr := "user=postgres dbname=academiq password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Begin()
+
+	log.Println("Connected to database")
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	// auth
+
+	http.HandleFunc("/auth/login", login)
+
+	http.ListenAndServe(":7301", nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
 }
